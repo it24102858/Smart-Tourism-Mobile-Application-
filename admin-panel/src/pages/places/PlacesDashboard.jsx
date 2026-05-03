30b61c7315e30e15ef89d24cff795d40615ca5ff
IT24102992 <IT24102992@my.sliit.lk>
2026-05-03 12:49:30 +0530

    initial commit

--- admin-panel/src/pages/places/PlacesDashboard.jsx
+++ admin-panel/src/pages/places/PlacesDashboard.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react' 
 import api from '../../api/axios'
 
 export default function PlacesDashboard() {
